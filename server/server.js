9c57078b8a10e2d388db0d7856ac95b65041c1e6
Edisson <edissonvelandia7@gmail.com>
2019-12-25 16:42:30 -0500

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 
+const colors = require('colors');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -10,40 +12,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
 
-    let body = req.body;
+        if (err) throw err;
 
-    if (body.nombre === undefined) {
+        console.log('Base de datos ONLINE'.green);
 
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
     });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto: ', process.env.PORT);
