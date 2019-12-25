171132a50561d8d78632fb326347d67d6a65abfd
Edisson <edissonvelandia7@gmail.com>
2019-12-25 17:19:56 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -13,14 +13,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // =======================================
 // BASE DE DATOS
 // =======================================
-
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://edissondb:wLL5MOXLCDBWouIE@cluster0-ji74d.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
-
 
 process.env.URLDB = urlDB;
